38d6be7fc979549fdf12acdb1e54a174dda89b15
Alessio Sclocco <alessio@sclocco.eu>
2013-09-30 12:04:42 +0200

    Updating the correctness test.

--- tests/Dedispersion.cpp
+++ tests/Dedispersion.cpp
@@ -43,6 +43,7 @@ using std::numeric_limits;
 #include <utils.hpp>
 #include <Shifts.hpp>
 #include <Dedispersion.hpp>
+#include <DedispersionCPU.hpp>
 using isa::utils::ArgumentList;
 using AstroData::Observation;
 using AstroData::readLOFAR;
@@ -51,149 +52,161 @@ using isa::OpenCL::GPUData;
 using isa::utils::same;
 using TDM::getShifts;
 using TDM::Dedispersion;
-using TDM::dedisperse;
+using TDM::dedispersion;
 
 typedef float dataType;
 const string typeName("float");
 
+// Common parameters
+const unsigned int nrBeams = 1;
+const unsigned int nrStations = 64;
+const unsigned int padding = 32;
+// LOFAR
+/*const float minFreq = 138.965f;
+const float channelBandwidth = 0.195f;
+const unsigned int nrSamplesPerSecond = 200000;
+const unsigned int nrChannels = 32;*/
+// Apertif
+const float minFreq = 1425.0f;
+const float channelBandwidth = 0.2929f;
+const unsigned int nrSamplesPerSecond = 20000;
+const unsigned int nrChannels = 1024;
+// DMs
+const unsigned int nrDMs = 256;
+const float firstDM = 0.0f;
+const float DMStep = 0.25f;
+
 
 int main(int argc, char *argv[]) {
-	unsigned int firstSecond = 0;
-	unsigned int nrSeconds = 0;
 	unsigned int clPlatformID = 0;
 	unsigned int clDeviceID = 0;
+	unsigned int nrSamplesPerBlock = 0;
+	unsigned int nrDMsPerBlock = 0;
+	unsigned int nrSamplesPerThread = 0;
+	unsigned int nrDMsPerThread = 0;
 	long long unsigned int wrongOnes = 0;
-	string headerFileName;
-	string dataFileName;
 	Observation< dataType > observation("DedispersionTest", typeName);
+	CLData< unsigned int > * shifts = 0;
+	CLData< dataType > * dispersedData = new CLData< dataType >("DispersedData", true);
+	CLData< dataType > * dedispersedData = new CLData< dataType >("DedispersedData", true);
+	CLData< dataType > * clDedispersedData = new CLData< dataType >("clDedispersedData", true);
 
 	try {
 		ArgumentList args(argc, argv);
 
-		firstSecond = args.getSwitchArgument< unsigned int >("-fs");
-		nrSeconds = args.getSwitchArgument< unsigned int >("-ns");
-		
 		clPlatformID = args.getSwitchArgument< unsigned int >("-opencl_platform");
 		clDeviceID = args.getSwitchArgument< unsigned int >("-opencl_device");
 
-		headerFileName = args.getSwitchArgument< string >("-header");
-		dataFileName = args.getSwitchArgument< string >("-data");
-
-		observation.setFirstDM(args.getSwitchArgument< float >("-dm_first"));
-		observation.setDMStep(args.getSwitchArgument< float >("-dm_step"));
-		observation.setNrDMs(args.getSwitchArgument< unsigned int >("-dm_number"));
-	}
-	catch ( exception &err ) {
+		nrSamplesPerBlock = args.getSwitchArgument< unsigned int >("-sb");
+		nrDMsPerBlock = args.getSwitchArgument< unsigned int >("-db");
+		nrSamplesPerThread = args.getSwitchArgument< unsigned int >("-st");
+		nrDMsPerThread = args.getSwitchArgument< unsigned int >("-dt");
+	} catch ( exception &err ) {
 		cerr << err.what() << endl;
 		return 1;
 	}
+
+	// Setup of the observation
+	observation.setPadding(padding);
+	observation.setNrBeams(nrStations);
+	observation.setNrStations(nrBeams);
+	observation.setMinFreq(minFreq);
+	observation.setMaxFreq(minFreq + (channelBandwidth * (nrChannels - 1)));
+	observation.setChannelBandwidth(channelBandwidth);
+	observation.setNrSamplesPerSecond(nrSamplesPerSecond);
+	observation.setNrChannels(nrChannels);
+	observation.setNrDMs(nrDMs);
+	observation.setFirstDM(firstDM);
+	observation.setDMStep(DMStep);
 	
-	// Load the observation data
-	vector< GPUData< dataType > * > *input = new vector< GPUData< dataType > * >(1);
-	readLOFAR(headerFileName, dataFileName, observation, *input, nrSeconds, firstSecond);
-
-	// Print some statistics
-	cout << fixed << setprecision(3) << endl;
-	cout << "Total seconds: \t\t" << observation.getNrSeconds() << endl;
-	cout << "Min frequency: \t\t" << observation.getMinFreq() << " MHz" << endl;
-	cout << "Max frequency: \t\t" << observation.getMaxFreq() << " MHz" << endl;
-	cout << "Nr. channels: \t\t" << observation.getNrChannels() << endl;
-	cout << "Channel bandwidth: \t" << observation.getChannelBandwidth() << " MHz" << endl;
-	cout << "Samples/second: \t" << observation.getNrSamplesPerSecond() << endl;
-	cout << "Min sample: \t\t" << observation.getMinValue() << endl;
-	cout << "Max sample: \t\t" << observation.getMaxValue() << endl;
-	cout << endl;
-
 	// Test
 	cl::Context *clContext = new cl::Context();
 	vector< cl::Platform > *clPlatforms = new vector< cl::Platform >();
 	vector< cl::Device > *clDevices = new vector< cl::Device >();
 	vector< vector< cl::CommandQueue > > *clQueues = new vector< vector < cl::CommandQueue > >();
-		
-	unsigned int nrSamplesPerChannel = 0;
-	unsigned int secondsToBuffer = 0;
-	GPUData< unsigned int > *shifts = getShifts(observation);
-	GPUData< dataType > *dispersedData = new GPUData< dataType >("DispersedData", true, true);
-	GPUData< dataType > *dedispersedData = new GPUData< dataType >("DedispersedData", true);
-	GPUData< dataType > *clDedispersedData = new GPUData< dataType >("clDedispersedData", true);
 
 	initializeOpenCL(clPlatformID, 1, clPlatforms, clContext, clDevices, clQueues);
-	
-	if ( ((observation.getNrSamplesPerSecond() + (*shifts)[((observation.getNrDMs() - 1) * observation.getNrPaddedChannels())]) % 4) != 0 ) {
-		nrSamplesPerChannel = (observation.getNrSamplesPerSecond() + (*shifts)[((observation.getNrDMs() - 1) * observation.getNrPaddedChannels())]) + (4 - ((observation.getNrSamplesPerSecond() + (*shifts)[((observation.getNrDMs() - 1) * observation.getNrPaddedChannels())]) % 4));
-	}
-	else {
+	shifts = getShifts(observation);
+
+	if ( ((observation.getNrSamplesPerSecond() + (*shifts)[((observation.getNrDMs() - 1) * observation.getNrPaddedChannels())]) % padding) != 0 ) {
+		nrSamplesPerChannel = (observation.getNrSamplesPerSecond() + (*shifts)[((observation.getNrDMs() - 1) * observation.getNrPaddedChannels())]) + (padding - ((observation.getNrSamplesPerSecond() + (*shifts)[((observation.getNrDMs() - 1) * observation.getNrPaddedChannels())]) % padding));
+	} else {
 		nrSamplesPerChannel = (observation.getNrSamplesPerSecond() + (*shifts)[((observation.getNrDMs() - 1) * observation.getNrPaddedChannels())]);
 	}
 	secondsToBuffer = static_cast< unsigned int >(ceil(static_cast< float >(nrSamplesPerChannel) / observation.getNrSamplesPerPaddedSecond()));
-	if ( nrSeconds < secondsToBuffer ) {
-		cerr << "Not enough seconds." << endl;
+		
+	// Allocate memory
+	dispersedData->allocateHostData(secondsToBuffer * observation.getNrChannels() * observation.getNrSamplesPerPaddedSecond());
+	dedispersedData->allocateHostData(observation.getNrDMs() * observation.getNrSamplesPerPaddedSecond());
+	clDedispersedData->allocateHostData(observation.getNrDMs() * observation.getNrSamplesPerPaddedSecond());
+
+	srand(time(NULL)));
+	for ( unsigned int channel = 0; channel < observation.getNrChannels(); channel++ ) {
+		for ( unsigned int sample = 0; sample < (secondsToBuffer * observation.getNrSamplesPerSecond()); sample++ ) {
+			dispersedData->setHostDataItem((channel * (secondsToBuffer * observation.getNrSamplesPerPaddedSecond())) + sample, static_cast< dataType >(rand() % 10));
+		}
+	}
+			
+	try {
+		shifts->setCLContext(clContext);
+		shifts->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		shifts->allocateDeviceData();
+		shifts->copyHostToDevice();
+
+		dispersedData->setCLContext(clContext);
+		dispersedData->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		dispersedData->setDeviceReadOnly();
+		dispersedData->allocateDeviceData();
+		dispersedData->copyHostToDevice();
+
+		clDedispersedData->setCLContext(clContext);
+		clDedispersedData->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		clDedispersedData->allocateDeviceData();
+	} catch ( OpenCLError err ) {
+		cerr << err.what() << endl;
 		return 1;
 	}
 	
-	// Allocate memory
-	shifts->setCLContext(clContext);
-	shifts->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	dispersedData->allocateHostData(secondsToBuffer * observation.getNrChannels() * observation.getNrSamplesPerPaddedSecond());
-	dispersedData->setCLContext(clContext);
-	dispersedData->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	dedispersedData->allocateHostData(observation.getNrDMs() * observation.getNrSamplesPerPaddedSecond());
-	clDedispersedData->allocateHostData(observation.getNrDMs() * observation.getNrSamplesPerPaddedSecond());
-	clDedispersedData->setCLContext(clContext);
-	clDedispersedData->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	
-	shifts->allocateDeviceData();
-	shifts->copyHostToDevice();
-	dispersedData->allocateDeviceData();
-	clDedispersedData->allocateDeviceData();
-	
 	// Generate kernel
-	Dedispersion< dataType > clDedisperse("Dedisperse", typeName);
-	clDedisperse.bindOpenCL(clContext, &(clDevices->at(clDeviceID)), &((clQueues->at(clDeviceID)).at(0)));
-	clDedisperse.setNrSamplesPerBlock(126);
-	clDedisperse.setNrDMsPerBlock(1);
-	clDedisperse.setNrSamplesPerThread(1);
-	clDedisperse.setNrDMsPerThread(1);
-	clDedisperse.setNrSamplesPerDispersedChannel(secondsToBuffer * observation.getNrSamplesPerPaddedSecond());
-	clDedisperse.setObservation(&observation);
-	clDedisperse.setShifts(shifts);
-	clDedisperse.generateCode();
+	try {
+		Dedispersion< dataType > clDedisperse("Dedisperse", typeName);
+		clDedisperse.bindOpenCL(clContext, &(clDevices->at(clDeviceID)), &((clQueues->at(clDeviceID)).at(0)));
+		clDedisperse.setNrSamplesPerBlock(nrSamplesPerBlock);
+		clDedisperse.setNrDMsPerBlock(nrDMsPerBlock);
+		clDedisperse.setNrSamplesPerThread(nrSamplesPerThread);
+		clDedisperse.setNrDMsPerThread(nrDMsPerThread);
+		clDedisperse.setNrSamplesPerDispersedChannel(secondsToBuffer * observation.getNrSamplesPerPaddedSecond());
+		clDedisperse.setObservation(&observation);
+		clDedisperse.setShifts(shifts);
+		clDedisperse.generateCode();
 
-	cout << clDedisperse.getCode() << endl;
-	cout << endl;
+		cout << clDedisperse.getCode() << endl;
+		cout << endl;
 
-	// Dedispersion loop
-	for ( unsigned int second = 0; second < nrSeconds - (secondsToBuffer - 1); second++ ) {
-		for ( unsigned int channel = 0; channel < observation.getNrChannels(); channel++ ) {
-			for ( unsigned int chunk = 0; chunk < secondsToBuffer; chunk++ ) {
-				memcpy(dispersedData->getRawHostDataAt((channel * secondsToBuffer * observation.getNrSamplesPerPaddedSecond()) + (chunk * observation.getNrSamplesPerSecond())), (input->at(second + chunk))->getRawHostDataAt(channel * observation.getNrSamplesPerPaddedSecond()), observation.getNrSamplesPerSecond() * sizeof(dataType));
-			}
-		}
-
-		dispersedData->copyHostToDevice();
 		clDedisperse(dispersedData, clDedispersedData);
 		clDedispersedData->copyDeviceToHost();
+	} catch ( OpenCLError err ) {
+		cerr << err.what() << endl;
+		return 1;
+	}
 
-		dedisperse(observation.getNrSamplesPerPaddedSecond() * secondsToBuffer, observation, dispersedData, dedispersedData, shifts);
+	dedispersion(observation.getNrSamplesPerPaddedSecond() * secondsToBuffer, observation, dispersedData->getHostData(), dedispersedData->getHostData(), shifts->getHostData());
 
-		for ( unsigned int dm = 0; dm < observation.getNrDMs(); dm++ ) {
-			for ( unsigned int sample = 0; sample < observation.getNrSamplesPerSecond(); sample++ ) {
-				if ( !same((*dedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample], (*clDedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample]) ) {
-					wrongOnes++;
+	for ( unsigned int dm = 0; dm < observation.getNrDMs(); dm++ ) {
+		for ( unsigned int sample = 0; sample < observation.getNrSamplesPerSecond(); sample++ ) {
+			if ( !same((*dedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample], (*clDedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample]) ) {
+				wrongOnes++;
 
-					cout << dm << " " << sample << " " << (*dedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample] << " " << (*clDedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample] << " " << (*dedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample] - (*clDedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample] << endl;
-				}
+				cout << dm << " " << sample << " " << (*dedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample] << " " << (*clDedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample] << " " << (*dedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample] - (*clDedispersedData)[(dm * observation.getNrSamplesPerPaddedSecond()) + sample] << endl;
 			}
 		}
 	}
+
 	cout << endl;
 
 	cout << "Wrong samples: " << wrongOnes << " (" << (wrongOnes * 100) / (static_cast< long long unsigned int >(observation.getNrSeconds()) * observation.getNrDMs() * observation.getNrSamplesPerSecond()) << "%)." << endl;
 	cout << endl;
-	cout << fixed << setprecision(6);
-	cout << "Kernel timing: " << clDedisperse.getTime() << " (total), " << clDedisperse.getTime() / (nrSeconds - (secondsToBuffer - 1)) << " (average)" << endl;
-	cout << endl;
-
+	
 	return 0;
 }
 
