54142942b726c2023d117ac790307c46081aca39
Alessio Sclocco <alessio@sclocco.eu>
2014-06-18 10:46:29 +0200

    Much less parameters needed for AVX generation.

--- src/generatorSIMD.cpp
+++ src/generatorSIMD.cpp
@@ -42,7 +42,6 @@ int main(int argc, char * argv[]) {
 	try {
     isa::utils::ArgumentList args(argc, argv);
 
-		observation.setPadding(args.getSwitchArgument< unsigned int >("-padding"));
     headerFilename = args.getSwitchArgument< std::string >("-header");
     avx = args.getSwitch("-avx");
     phi = args.getSwitch("-phi");
@@ -50,25 +49,13 @@ int main(int argc, char * argv[]) {
       throw isa::Exceptions::EmptyCommandLine();
     }
 		maxItemsPerThread = args.getSwitchArgument< unsigned int >("-max_items");
-		observation.setMinFreq(args.getSwitchArgument< float >("-min_freq"));
-		observation.setChannelBandwidth(args.getSwitchArgument< float >("-channel_bandwidth"));
-		observation.setNrSamplesPerSecond(args.getSwitchArgument< unsigned int >("-samples"));
-		observation.setNrChannels(args.getSwitchArgument< unsigned int >("-channels"));
-		observation.setNrDMs(args.getSwitchArgument< unsigned int >("-dms"));
-		observation.setFirstDM(args.getSwitchArgument< float >("-dm_first"));
-		observation.setDMStep(args.getSwitchArgument< float >("-dm_step"));
-		observation.setMaxFreq(observation.getMinFreq() + (observation.getChannelBandwidth() * (observation.getNrChannels() - 1)));
 	} catch ( isa::Exceptions::EmptyCommandLine &err ) {
-		std::cerr << argv[0] << " -padding ... -header ... [-avx] [-phi] -max_items ... -min_freq ... -channel_bandwidth ... -samples ... -channels ... -dms ... -dm_first ... -dm_step ..." << std::endl;
+		std::cerr << argv[0] << " -header ... [-avx] [-phi] -max_items ..." << std::endl;
 		return 1;
 	} catch ( std::exception &err ) {
 		std::cerr << err.what() << std::endl;
 		return 1;
 	}
-  std::vector< unsigned int > * shifts = PulsarSearch::getShifts(observation);
-  observation.setNrSamplesPerDispersedChannel(observation.getNrSamplesPerSecond() + (*shifts)[((observation.getNrDMs() - 1) * observation.getNrPaddedChannels())]);
-  delete shifts;
-
   std::string underscore("_");
   std::string * defineName = isa::utils::replace(&headerFilename, ".", underscore);
   std::ofstream headerFile(headerFilename);
