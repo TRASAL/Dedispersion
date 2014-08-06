e4c0293e3cc18f1b6a060339dfb3fdd1540b044e
Alessio Sclocco <alessio@sclocco.eu>
2014-08-06 13:49:09 +0200

    The copyright for this new version is 2014.

--- tuning/Dedispersion.cpp
+++ tuning/Dedispersion.cpp
@@ -1,4 +1,4 @@
-// Copyright 2013 Alessio Sclocco <a.sclocco@vu.nl>
+// Copyright 2014 Alessio Sclocco <a.sclocco@vu.nl>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
