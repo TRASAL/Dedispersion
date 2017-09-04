8e094ef98eafb1d683aa125a70e3dba0376a72cb
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-09-04 16:49:49 +0200

    Added header files.

--- include/configuration.hpp
+++ include/configuration.hpp
@@ -1,8 +1,11 @@
 
+#include <cstdint>
+#include <string>
+
 #pragma once
 
 // Define the data types
-typedef std::uint8_t inputDataType;
+typedef uint8_t inputDataType;
 std::string inputDataName("uchar");
 typedef float intermediateDataType;
 std::string intermediateDataName("float");
